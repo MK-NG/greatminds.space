03d77d0c13593f517a4b6af027a23730cfff32e1
Mike Knight <38420388+MK-NG@users.noreply.github.com>
2022-03-22 17:56:43 +0000

    netlify fix?

--- src/js/script.js
+++ src/js/script.js
@@ -114,17 +114,52 @@ menu.addEventListener("mouseleave", () =>
 
 // 
 
-const s = document.querySelector('.Subscribe')
-const b = document.querySelector('button')
-b.addEventListener('click', (e) => {
-  e.preventDefault()
-  s.classList.toggle('Subscribe--loading')
-  setTimeout(() => {
-     s.classList.remove('Subscribe--loading')
-     s.classList.toggle('Subscribe--complete')
-  }, 2000)
+// const s = document.querySelector('.Subscribe')
+// const b = document.querySelector('button')
+// b.addEventListener('click', (e) => {
+//   e.preventDefault()
+
   
-  setTimeout(() => {
-     s.classList.remove('Subscribe--complete')
-  }, 20000)
-})
+
+//   s.classList.toggle('Subscribe--loading')
+//   setTimeout(() => {
+//     s.classList.remove('Subscribe--loading')
+//     s.classList.toggle('Subscribe--complete')
+//   }, 2000)
+  
+//   setTimeout(() => {
+//     s.classList.remove('Subscribe--complete')
+//   }, 20000)
+
+  
+// })
+
+
+
+ const s = document.querySelector('.Subscribe')
+ const b = document.querySelector('button')
+
+signup.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const formData = new FormData(signup);
+  fetch(signup.getAttribute('sub'), {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+    body: new URLSearchParams(formData).toString()
+  })
+  .then(res => {
+    s.classList.toggle('Subscribe--loading')
+    setTimeout(() => {
+      s.classList.remove('Subscribe--loading')
+      s.classList.toggle('Subscribe--complete')
+    }, 2000)
+    
+    setTimeout(() => {
+      s.classList.remove('Subscribe--complete')
+    }, 20000)
+  });
+});
