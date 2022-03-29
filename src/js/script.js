befd9507daa91de1dbb47a4ef470697c93847a31
Mike Knight <38420388+MK-NG@users.noreply.github.com>
2022-03-29 19:09:12 +0100

    remove header  fadeout

--- src/js/script.js
+++ src/js/script.js
@@ -60,35 +60,6 @@ setInterval(function () {
 
 
 
-//  HEADER FADE OUT
-
-
-
-
-// 
-
-var header = $('header');
-var range = 200;
-
-$(window).on('scroll', function () {
-  
-  var scrollTop = $(this).scrollTop(),
-      height = header.outerHeight(),
-      offset = height / 2,
-      calc = 1 - (scrollTop - offset + range) / range;
-
-  header.css({ 'opacity': calc });
-
-  if (calc > '1') {
-    header.css({ 'opacity': 1 });
-  } else if ( calc < '0' ) {
-    header.css({ 'opacity': 0 });
-  }
-  
-});
-
-// 
-
 
 //  NAVIAGTION 
 
