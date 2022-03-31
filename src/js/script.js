127cfa1f9de865f8f01f65523570e39adcae81f0
Mike Knight <38420388+MK-NG@users.noreply.github.com>
2022-03-31 15:32:56 +0100

    add dark mode

--- src/js/script.js
+++ src/js/script.js
@@ -150,3 +150,34 @@ const observer = new IntersectionObserver(entries => {
 observer.observe(document.querySelector('.underline'));
 
 observer.observe(document.querySelector('.underline-2'));
+
+
+
+// 
+
+
+
+//  DARK MODE  
+
+
+// 
+
+
+var checkbox = document.querySelector('input[name=mode]');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+})
+
+let trans = () => {
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+      document.documentElement.classList.remove('transition');
+  }, 1000)
+}
