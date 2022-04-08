b401766bfc3b028044d4961ca259f1ce9e43174d
Mike Knight <38420388+MK-NG@users.noreply.github.com>
2022-04-08 19:11:40 +0100

    revert to last working version

--- src/js/script.js
+++ src/js/script.js
@@ -300,12 +300,3 @@ Array.from(
 		);
 	}
 );
-
-var seeMore = document.getElementById("seeMore");
-
-
-seeMore.onclick = function() {
-  document.querySelector('.intro').scrollIntoView({ behavior: 'smooth' }); 
-  return false;
-}
-
